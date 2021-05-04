2a8bf715801797817780fac252fe14c47f82b083
Kuroikuma <73002782+Kuroikuma@users.noreply.github.com>
2021-05-03 21:14:04 -0600

    borrando todo de nuevo

--- src/app/page/client/customization/customization.js
+++
@@ -1,7 +0,0 @@
-import { React } from "react";
-import CustomizationView from "./customization.view";
-function Customization() {
-  return <CustomizationView />;
-}
-
-export default Customization;
