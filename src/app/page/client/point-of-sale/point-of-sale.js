6c4ff23ee513d734c7855314d9d9143da2fd3e7a
Kuroikuma <73002782+Kuroikuma@users.noreply.github.com>
2021-05-21 20:25:45 -0600

    agregando punto de venta para cliente

--- src/app/page/client/point-of-sale/point-of-sale.js
+++ src/app/page/client/point-of-sale/point-of-sale.js
@@ -4,49 +4,86 @@ import PointOfSaleView from "./point-of-sale.view";
 
 function PointOfSale() {
   const data = [];
+  const sale = [];
+  sale.push({
+    title: "pizza de queso",
+    amount: 2,
+    price: 500,
+    image: pizza1,
+  });
+  sale.push({
+    title: "pizza de queso",
+    amount: 2,
+    price: 500,
+    image: pizza1,
+  });
+  sale.push({
+    title: "pizza de queso",
+    amount: 2,
+    price: 500,
+    image: pizza1,
+  });
+  sale.push({
+    title: "pizza de queso",
+    amount: 2,
+    price: 500,
+    image: pizza1,
+  });
+  sale.push({
+    title: "pizza de queso",
+    amount: 2,
+    price: 500,
+    image: pizza1,
+  });
+  sale.push({
+    title: "pizza de queso",
+    amount: 2,
+    price: 500,
+    image: pizza1,
+  });
+  data.push({
+    ingredients:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    title: "pizza de queso",
+    price: 500,
+    image: pizza1,
+  });
+  data.push({
+    ingredients:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    title: "pizza de queso",
+    price: 500,
+    image: pizza1,
+  });
+  data.push({
+    ingredients:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    title: "pizza de queso",
+    price: 500,
+    image: pizza1,
+  });
+  data.push({
+    ingredients:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    title: "pizza de queso",
+    price: 500,
+    image: pizza1,
+  });
+  data.push({
+    ingredients:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    title: "pizza de queso",
+    price: 500,
+    image: pizza1,
+  });
+  data.push({
+    ingredients:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    title: "pizza de queso",
+    price: 500,
+    image: pizza1,
+  });
 
-  data.push({
-    ingredients:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-    title: "pizza de queso",
-    precio: 500,
-    image: pizza1,
-  });
-  data.push({
-    ingredients:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-    title: "pizza de queso",
-    precio: 500,
-    image: pizza1,
-  });
-  data.push({
-    ingredients:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-    title: "pizza de queso",
-    precio: 500,
-    image: pizza1,
-  });
-  data.push({
-    ingredients:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-    title: "pizza de queso",
-    precio: 500,
-    image: pizza1,
-  });
-  data.push({
-    ingredients:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-    title: "pizza de queso",
-    precio: 500,
-    image: pizza1,
-  });
-  data.push({
-    ingredients:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-    title: "pizza de queso",
-    precio: 500,
-    image: pizza1,
-  });
-  return <PointOfSaleView data={data} />;
+  return <PointOfSaleView data={data} sale={sale} />;
 }
 export default PointOfSale;
--- src/app/page/client/point-of-sale/point-of-sale.view.jsx
+++ src/app/page/client/point-of-sale/point-of-sale.view.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import pizza1 from "../../../../assets/image/MenuPizzas/menupizza1.jpg";
+import image from "../../../../assets/image/logo.png";
 import "./point-of-sale.style.css";
 
 const PointOfSaleView = (props) => {
-  const { data } = props;
+  const { data, sale } = props;
   return (
     <>
       <div className="container-order">
-        <div className="bar__order"></div>
+        <div className="bar__order">
+          <img src={image} />
+        </div>
         <div className="data__order">
           <div className="product__data">
             <h4>PRODUCTO</h4>
@@ -28,7 +32,7 @@ const PointOfSaleView = (props) => {
           </div>
           <div className="order__data">
             <div className="order__data__bar">
-              <h3 className="bar__title">HAZ TU PEDIDO!</h3>
+              <h2 className="bar__title">HAZ TU PEDIDO!</h2>
               <input className="bar__input" type="text" />
             </div>
             <div className="order">
@@ -43,18 +47,41 @@ const PointOfSaleView = (props) => {
                     </div>
                     <div className="ingredients">
                       <h3>Ingredientes</h3>
-
                       <p>{data.ingredients}</p>
                     </div>
                     <div className="price">
-                      <div>C${data.precio}</div>
+                      <div>C${data.price}</div>
                     </div>
                   </div>
                 ))}
               </div>
             </div>
           </div>
-          <div className="currentOrder__data"></div>
+          <div className="currentOrder">
+            <div className="title__currentOrder">
+              <h2>Current order</h2>
+            </div>
+            <div className="products__currentOrder">
+              {sale.map((sale) => (
+                <div className="saleItems__products">
+                  <img src={sale.image} />
+                  <p>{sale.title}</p>
+                  <p>x{sale.amount}</p>
+                  <p>C${sale.price}</p>
+                </div>
+              ))}
+            </div>
+            <div className="currentOrden__transaction">
+              <div className="grantTotal__transaction">
+                <h4 className="title__grantTotal">Grant Total</h4>
+                <p className="detail__grantTotal">C$300</p>
+              </div>
+              <div className="transaction">
+                <button className="clear__transaction">Clear all</button>
+                <button className="check__transaction">Check out</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
